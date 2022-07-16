9c86f4dff777c4133033a9d95784669c8763be96
Allan <github email address>
2022-07-16 15:06:21 +0300

    import and export lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/about";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+      <Home/>
+      <About/>
     </div>
   );
 }
